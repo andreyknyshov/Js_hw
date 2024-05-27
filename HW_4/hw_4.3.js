79f80be1bd38a27d4d894a74abc46e8a6df53888
Andrey Knyshov <andrey.knyshov.qa@gmail.com>
2024-05-27 09:41:08 +0300

    Update HW_4/hw_4.3.js
    Co-authored-by: Volodymyr Lazurenko <103571439+VladimirLQA@users.noreply.github.com>

--- HW_4/hw_4.3.js
+++ HW_4/hw_4.3.js
@@ -4,7 +4,7 @@
 const age = +prompt ('Enter your age: '); 
 const age_2 = 18; 
 const age_3 = 60; 
-if (isNaN(age) || (age < 1)) {
+if (isNaN(age)) {
     alert('Error, try again '); 
 }   else if (age < age_2) { 
      alert('You don’t have access cause your age is ' + age + ' It’s less then '); 
